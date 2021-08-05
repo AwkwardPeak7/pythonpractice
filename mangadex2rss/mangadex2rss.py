5cefe38aaec999b162906d73f714ed05d08bb40c
Mubashir Haroon <mubashirmusab@gmail.com>
2021-08-05 13:54:22 +0500

    mangadex2rss first commit

---
+++ mangadex2rss/mangadex2rss.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import sys,json,requests
+import xml.etree.cElementTree as e
+
+try:
+    manga_id = sys.argv[1]
+except IndexError:
+    print('No Manga ID Specified', file=sys.stderr)
+    exit(1)
+
+try:
+    lang = sys.argv[2]
+except IndexError:
+    lang = "en"
+
+manga_info_url = "https://api.mangadex.org/manga/" + manga_id
+manga_feed_url = "https://api.mangadex.org/manga/{}/feed?translatedLanguage[]={}&order[chapter]=desc".format(manga_id,lang)
