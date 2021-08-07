0033c403d2cc536de4ec5d0fa897eec9f25d34ca
Mubashir Haroon <mubashirmusab@gmail.com>
2021-08-07 12:20:36 +0500

    Revert "pretty print"
    This reverts commit 2c5be9174f5393eecd58beca470e3aa94d5d7351.

--- mangadex2rss/mangadex2rss.py
+++ mangadex2rss/mangadex2rss.py
@@ -13,7 +13,6 @@ import sys,json,requests
 import xml.etree.cElementTree as ET
 from io import BytesIO
 import uuid
-import xml.dom.minidom
 
 try:
     manga_id = str(uuid.UUID(sys.argv[1]))
@@ -68,4 +67,4 @@ for result in manga_feed["results"]:
 f = BytesIO()
 rss_feed = ET.ElementTree(root)
 rss_feed.write(f, encoding='utf-8', xml_declaration=True)
-print(xml.dom.minidom.parseString(f.getvalue().decode('utf-8')).toprettyxml())
+print(f.getvalue().decode('utf-8'))
